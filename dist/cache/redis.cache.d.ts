e111669d65e4e2b52551c5b519fbd1c5fc6a21e2
Mogeli Lortkipanidze <lortkipanidzemogeli@gmail.com>
2020-10-19 02:18:18 +0400

    added node-cache module types in dev dependencies

--- dist/cache/redis.cache.d.ts
+++ dist/cache/redis.cache.d.ts
@@ -2,7 +2,7 @@ export declare class RedisModule {
     redis: any;
     constructor(clientOpts?: any);
     connect(): Promise<void>;
-    get(key: any): Promise<any>;
-    set(key: any, data: any, ex?: number): Promise<any>;
+    get(key: any): Promise<string>;
+    set(key: any, data: any, ex?: number): Promise<string>;
     del(key: any): Promise<void>;
 }
--- dist/logs.module.d.ts
+++ dist/logs.module.d.ts
@@ -1,5 +1,6 @@
-export declare const RequestLog: (req: any) => Promise<void>;
-export declare const BadResponseLog: (req: any, data: {
+import { Request } from 'express';
+export declare const RequestLog: (req: Request) => Promise<void>;
+export declare const BadResponseLog: (req: Request, data: {
     target: string;
     content: {};
 }) => Promise<void>;
